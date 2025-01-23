5b0e1dae492876d0a07be85857abb9ebbbc5ab3a
RubenPerezCastrillo <ruben.perez.castrillo@gmail.com>
2025-01-23 12:13:44 +0000

    Solved problems with relations and tablenames

--- src/models.py
+++ src/models.py
@@ -8,7 +8,7 @@ from eralchemy2 import render_er
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'User'
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
@@ -16,67 +16,67 @@ class User(Base):
     name = Column(String(150), nullable=False)
     email = Column(String(150), nullable=False, unique=True)
     password = Column(String(150))
-    favorite = relationship('Favorite', back_populates='User')
+    favorite = relationship('Favorites', back_populates='user')
 
 class Favorites(Base):
-    __tablename__ = 'Favorites'
+    __tablename__ = 'favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
 
-    user_id = Column(Integer, ForeignKey('User.id'))
-    user = relationship('User', back_populates='Favorite')
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='favorites')
 
-    character_id = Column(Integer, ForeignKey('Character.id'))
-    character = relationship('Character', back_populates='Favorite')
+    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+    character = relationship('Character', back_populates='favorites')
 
-    planet_id = Column(Integer, ForeignKey('Planet.id'))
-    planet = relationship('Planet', back_populates='Favorite')
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    planet = relationship('Planet', back_populates='favorites')
 
-    vehicle_id = Column(Integer, ForeignKey('Vehicle.id'))
-    vehicle = relationship('Vehicle', back_populates='Favorite')
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'), nullable=True)
+    vehicle = relationship('Vehicle', back_populates='favorites')
 
 class Character(Base):
-    __tablename__ = 'Character'
+    __tablename__ = 'character'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(150), nullable=False)
-    gender = Column(String(15))
-    birth_date = Column(String(100))
+    gender = Column(String(15), nullable=True)
+    birth_date = Column(String(100), nullable=True)
     description = Column(String(500), nullable=False)
     url = Column(String(150), nullable=False)
 
-    planet_id = Column(Integer, ForeignKey('Planet.id'))
-    planet = relationship('Planet', back_populates='Character')
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    planet = relationship('Planet', back_populates='characters')
 
-    favorite = relationship('Favorite', back_populates='Character')
-    vehicle = relationship('Vehicle', back_populates='Character')
+    favorite = relationship('Favorite', back_populates='character')
+    vehicle = relationship('Vehicle', back_populates='character')
 
 class Planet(Base):
-    __tablename__ = 'Planet'
+    __tablename__ = 'planet'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(150), nullable=False)
     description = Column(String(500), nullable=False)
     population = Column(Integer, nullable=False)
-    climate = Column(String(50))
+    climate = Column(String(50), nullable=True)
 
-    favorite = relationship('Favorite', back_populates='Planet')
-    character = relationship('Character', back_populates='Planet')
+    favorite = relationship('Favorites', back_populates='planet')
+    character = relationship('Character', back_populates='planet')
 
 class Vehicle(Base):
-    __tablename__ = 'Vehicle'
+    __tablename__ = 'vehicle'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(150), nullable=False)
-    model = Column(String(100))
+    model = Column(String(100), nullable=True)
     description = Column(String(500), nullable=False)
     capacity = Column(Integer, nullable=False)
 
-    character_id = Column(Integer, ForeignKey('Character.id'))
-    character = relationship('Character', back_populates='Vehicle')
+    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+    character = relationship('Character', back_populates='vehicle')
 
-    favorite = relationship('Favorite', back_populates='Vehicle')
+    favorite = relationship('Favorites', back_populates='vehicle')
 
     def to_dict(self):
         return {}
